a95b4416fd344dbc22ef355e1bcf805ad43b323c
Alfredo Mungo <alfredo.mungo@protonmail.ch>
2021-01-31 02:42:18 +0000

    Removed scripts folder

--- scripts/init.cmake
+++
@@ -1,9 +0,0 @@
-get_filename_component(PROJ_DIR ${CMAKE_CURRENT_LIST_DIR} DIRECTORY)
-
-execute_process(
-    COMMAND ${CMAKE_COMMAND}
-        -S ${PROJ_DIR}
-        -B build
-        -D CMAKE_EXPORT_COMPILE_COMMANDS=1
-        -G Ninja
-)
